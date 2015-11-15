b6c3f951d32cd3a33f06cc535b926967dc84a1ff
sridhar <karanamsridhar@gmail.com>
2015-11-15 08:11:52 -0600

    Webservice base working

--- warehouseBinSensorDataService/src/main/java/com/yash/iot/service/endpoint/WarehouseBinSensorDataServiceEndpoint.java
+++ warehouseBinSensorDataService/src/main/java/com/yash/iot/service/endpoint/WarehouseBinSensorDataServiceEndpoint.java
@@ -1,10 +1,14 @@
 package com.yash.iot.service.endpoint;
 
+import javax.xml.bind.JAXBElement;
+import javax.xml.namespace.QName;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.ws.server.endpoint.annotation.Endpoint;
 import org.springframework.ws.server.endpoint.annotation.PayloadRoot;
 import org.springframework.ws.server.endpoint.annotation.RequestPayload;
 import org.springframework.ws.server.endpoint.annotation.ResponsePayload;
+
 import com.yash.iot.domain.GetWarehouseBinSensorDataRequest;
 import com.yash.iot.domain.GetWarehouseBinSensorDataResponse;
 import com.yash.iot.service.IfcWarehouseBinSensorDataService;
@@ -16,7 +20,10 @@ public class WarehouseBinSensorDataServiceEndpoint {
 	private IfcWarehouseBinSensorDataService ifcWarehouseBinSensorDataService;
 	
 	@PayloadRoot(localPart="GetWarehouseBinSensorDataRequest", namespace="http://www.iot.yash.com/WarehouseBinSensorData")
-	public @ResponsePayload GetWarehouseBinSensorDataResponse getWarehouseBinSensorData(@RequestPayload GetWarehouseBinSensorDataRequest request){
-		return ifcWarehouseBinSensorDataService.getWarehouseBinSensorData(request);
+	public @ResponsePayload JAXBElement<GetWarehouseBinSensorDataResponse> getWarehouseBinSensorData(@RequestPayload JAXBElement<GetWarehouseBinSensorDataRequest> request){
+		GetWarehouseBinSensorDataRequest warehouseBinSensorDataRequest = request.getValue();
+		GetWarehouseBinSensorDataResponse response = ifcWarehouseBinSensorDataService.getWarehouseBinSensorData(warehouseBinSensorDataRequest);
+		JAXBElement<GetWarehouseBinSensorDataResponse> responseElement = new JAXBElement<GetWarehouseBinSensorDataResponse>(new QName("GetWarehouseBinSensorDataResponse"), GetWarehouseBinSensorDataResponse.class, response);
+		return responseElement;
 	}
 }
